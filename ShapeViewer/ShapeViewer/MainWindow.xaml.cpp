4ffff006933550c92e89806f2fefcac2809eb1f2
Owen Meng <owen.meng@autodesk.com>
2024-04-09 16:17:22 +0800

    Add title

--- ShapeViewer/ShapeViewer/MainWindow.xaml.cpp
+++ ShapeViewer/ShapeViewer/MainWindow.xaml.cpp
@@ -10,6 +10,7 @@
 using namespace winrt;
 using namespace Microsoft::UI::Input;
 using namespace Microsoft::UI::Xaml;
+using namespace Microsoft::UI::Xaml::Data;
 using namespace Microsoft::UI::Xaml::Media;
 using namespace Microsoft::UI::Composition;
 
@@ -24,6 +25,57 @@ MainWindow::~MainWindow() = default;
 void MainWindow::InitializeComponent()
 {
     MainWindowT::InitializeComponent();
+
+    _Timer.Reset();
+}
+
+winrt::event_token MainWindow::PropertyChanged(PropertyChangedEventHandler const& handler)
+{
+    return _PropertyChanged.add(handler);
+}
+
+void MainWindow::PropertyChanged(winrt::event_token const& token) noexcept
+{
+    _PropertyChanged.remove(token);
+}
+
+winrt::hstring MainWindow::WindowTitle()
+{
+    return _WindowTitle;
+}
+
+void MainWindow::WindowTitle(hstring const& value)
+{
+    if (_WindowTitle != value)
+    {
+        if (_WindowTitle != value)
+            _WindowTitle = value;
+        _PropertyChanged(*this, PropertyChangedEventArgs{L"WindowTitle"});
+    }
+}
+
+void MainWindow::CalculateFrameStats()
+{
+    static int frameCount = 0;
+    static double timeEplapsed = 0.;
+
+    frameCount++;
+
+    if ((_Timer.TotalTimeInSeconds() - timeEplapsed) >= 1.)
+    {
+        double fps = frameCount;
+        double mspf = 1000. / fps;
+
+        std::wstring fpsStr = std::to_wstring(fps);
+        std::wstring mspfStr = std::to_wstring(mspf);
+
+        std::wstring windowText = L"ShapeViwer....fps: " + fpsStr + L"   mspf: " + mspfStr;
+
+        Title(windowText.c_str());
+
+        frameCount = 0;
+        timeEplapsed += 1.;
+    }
 }
 
 } // namespace winrt::ShapeViewer::implementation
@@ -35,13 +87,14 @@ void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_Loaded(
     _d3dApp->InitDirect3D();
     _d3dApp->OnResize(swapChainPanel().ActualWidth(), swapChainPanel().ActualHeight());
 
-    CompositionTarget::Rendering({this, &MainWindow::swapChainPanel_RenderingHandler});
+    _RenderingToken = CompositionTarget::Rendering({this, &MainWindow::swapChainPanel_RenderingHandler});
 }
 
 void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_Unloaded(
     winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
 {
     _d3dApp.reset(nullptr);
+    CompositionTarget::Rendering(_RenderingToken);
 }
 
 void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_SizeChanged(
@@ -57,10 +110,11 @@ void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_SizeChanged(
 void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_RenderingHandler(
     winrt::Windows::Foundation::IInspectable const& sender, winrt::Windows::Foundation::IInspectable const& args)
 {
-    if (!_d3dApp)
+    if (_WindowClosed || !_d3dApp)
     {
         return;
     }
+    CalculateFrameStats();
     _d3dApp->Update();
     _d3dApp->Draw();
 }
@@ -76,7 +130,7 @@ void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_PointerPress
 
     swapChainPanel().CapturePointer(e.Pointer());
 
-    WPARAM btnState;
+    WPARAM btnState = 0;
     auto pointerPoint = e.GetCurrentPoint(swapChainPanel());
     if (pointerPoint.Properties().IsLeftButtonPressed())
     {
@@ -106,7 +160,7 @@ void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_PointerMoved
     auto pointerPoint = e.GetCurrentPoint(swapChainPanel());
     auto position = pointerPoint.Position();
 
-    WPARAM btnState;
+    WPARAM btnState = 0;
     if (pointerPoint.Properties().IsLeftButtonPressed())
     {
         btnState |= MK_LBUTTON;
@@ -118,4 +172,28 @@ void winrt::ShapeViewer::implementation::MainWindow::swapChainPanel_PointerMoved
     }
 
     _d3dApp->OnMouseMove(btnState, position.X, position.Y);
+}
+
+void winrt::ShapeViewer::implementation::MainWindow::Window_Activated(
+    winrt::Windows::Foundation::IInspectable const& sender,
+    winrt::Microsoft::UI::Xaml::WindowActivatedEventArgs const& args)
+{
+    if (args.WindowActivationState() == WindowActivationState::Deactivated)
+    {
+        _Timer.Stop();
+    }
+    else if (
+        args.WindowActivationState() == WindowActivationState::PointerActivated ||
+        args.WindowActivationState() == WindowActivationState::CodeActivated)
+    {
+        _Timer.Start();
+    }
+}
+
+
+
+void winrt::ShapeViewer::implementation::MainWindow::Window_Closed(
+    winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::WindowEventArgs const& args)
+{
+    _WindowClosed = true;
 }
--- ShapeViewer/ShapeViewer/MainWindow.xaml.h
+++ ShapeViewer/ShapeViewer/MainWindow.xaml.h
@@ -3,6 +3,7 @@
 #include "MainWindow.g.h"
 
 #include "d3dApp.h"
+#include "GameTimer.h"
 
 namespace winrt::ShapeViewer::implementation
 {
@@ -18,9 +19,24 @@ struct MainWindow : MainWindowT<MainWindow>
 
     void InitializeComponent();
 
+    void CalculateFrameStats();
+
+    winrt::event_token PropertyChanged(winrt::Microsoft::UI::Xaml::Data::PropertyChangedEventHandler const& handler);
+    void PropertyChanged(winrt::event_token const& token) noexcept;
+
+    winrt::hstring WindowTitle();
+    void WindowTitle(hstring const& value);
+
   private:
     std::unique_ptr<D3DApp> _d3dApp;
     winrt::event_token _RenderingToken;
+    GameTimer _Timer;
+
+    winrt::hstring _WindowTitle; 
+
+    bool _WindowClosed = false;
+
+    winrt::event<Microsoft::UI::Xaml::Data::PropertyChangedEventHandler> _PropertyChanged;
 
   public:
     void swapChainPanel_Loaded(
@@ -42,6 +58,12 @@ struct MainWindow : MainWindowT<MainWindow>
     void swapChainPanel_PointerMoved(
         winrt::Windows::Foundation::IInspectable const& sender,
         winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+    void Window_Activated(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Microsoft::UI::Xaml::WindowActivatedEventArgs const& args);
+    void Window_Closed(
+        winrt::Windows::Foundation::IInspectable const& sender,
+        winrt::Microsoft::UI::Xaml::WindowEventArgs const& args);
 };
 } // namespace winrt::ShapeViewer::implementation
 
